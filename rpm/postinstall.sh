0880c6a64389cf56725619ddd01866dc73ab4df0
Brandon Beck <brandon.beck@bazaarvoice.com>
2013-08-23 14:16:19 -0500

    More deploy updates

--- rpm/postinstall.sh
+++ rpm/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $1 -eq 1 ]; then
   HOME=$(getent passwd "puzzlebox" | awk -v FS=: '{print $6}')
-  sudo -u "puzzlebox" bash -c "cd ${HOME}; npm install"
+  su -c "/bin/bash -c 'cd ${HOME}; npm install'" puzzlebox
 
   /sbin/chkconfig --add puzzlebox
 fi
