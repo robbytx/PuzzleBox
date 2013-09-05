e314238a2c4d42d74dbb46c91f1c2efe91ccacd4
Michael Pratt <urthen@gmail.com>
2013-09-05 15:57:27 -0500

    fixing index

--- web/app/controllers/indexController.js
+++ web/app/controllers/indexController.js
@@ -1,3 +1,3 @@
 exports.renderIndex = function(req, res) {
-	res.render('index', {add_name: req.user.name == "Anonymous"});
+	res.render('index', {add_name: req.user && req.user.name == "Anonymous"});
 };
